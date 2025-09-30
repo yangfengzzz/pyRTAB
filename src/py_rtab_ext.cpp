073b9180afe70a22e4f61bbfb08e26922a8b69ed
yangfengzzz <yangfengzzz@hotmail.com>
2025-10-01 07:34:57 +0800

    pass python test

--- src/py_rtab_ext.cpp
+++ src/py_rtab_ext.cpp
@@ -14,6 +14,7 @@
 
 #include <rtabmap/core/Rtabmap.h>
 #include <rtabmap/core/Memory.h>
+#include "cv_typecaster.h"
 
 namespace nb = nanobind;
 using namespace rtabmap;
@@ -29,7 +30,7 @@ void bindStereoCameraModel(nb::module_ &m);
 void bindStatistics(nb::module_ &m);
 void bindSignature(nb::module_ &m);
 void bindLink(nb::module_ &m);
-void bindMemory(nb::module_& m);
+void bindMemory(nb::module_ &m);
 void bindFeature2D(nb::module_ &m);
 void bindVWDictionary(nb::module_ &m);
 void bindVisualWord(nb::module_ &m);
@@ -69,7 +70,7 @@ NB_MODULE(rtab_ext, m) {
                  "parameters"_a, "databasePath"_a, "loadDatabaseParameters"_a)
             .def("init", nb::overload_cast<const std::string &, const std::string &, bool>(&Rtabmap::init),
                  "configFile"_a, "databasePath"_a, "loadDatabaseParameters"_a)
-            .def("close", &Rtabmap::close)
+            .def("close", &Rtabmap::close, "databaseSaved"_a = true, "ouputDatabasePath"_a = "")
             .def("getWorkingDir", &Rtabmap::getWorkingDir)
             .def("isRGBDMode", &Rtabmap::isRGBDMode)
             .def("getLoopClosureId", &Rtabmap::getLoopClosureId)
@@ -84,37 +85,38 @@ NB_MODULE(rtab_ext, m) {
             .def("getWeights", &Rtabmap::getWeights)
             .def("getTotalMemSize", &Rtabmap::getTotalMemSize)
             .def("getLastProcessTime", &Rtabmap::getLastProcessTime)
-            .def("isInSTM", &Rtabmap::isInSTM)
+            .def("isInSTM", &Rtabmap::isInSTM, "locationId"_a)
             .def("isIDsGenerated", &Rtabmap::isIDsGenerated)
             .def("getStatistics", &Rtabmap::getStatistics)
             .def("getLocalOptimizedPoses", &Rtabmap::getLocalOptimizedPoses)
             // .def("getLocalConstraints", &Rtabmap::getLocalConstraints)
-            .def("getPose", &Rtabmap::getPose)
+            .def("getPose", &Rtabmap::getPose, "locationId"_a)
             .def("getMapCorrection", &Rtabmap::getMapCorrection)
             .def("getMemory", &Rtabmap::getMemory)
             .def("getGoalReachedRadius", &Rtabmap::getGoalReachedRadius)
             .def("getLocalRadius", &Rtabmap::getLocalRadius)
             .def("getLastLocalizationPose", &Rtabmap::getLastLocalizationPose)
             .def("getTimeThreshold", &Rtabmap::getTimeThreshold)
-            .def("setTimeThreshold", &Rtabmap::setTimeThreshold)
+            .def("setTimeThreshold", &Rtabmap::setTimeThreshold, "maxTimeAllowed"_a)
             .def("getMemoryThreshold", &Rtabmap::getMemoryThreshold)
-            .def("setMemoryThreshold", &Rtabmap::setMemoryThreshold)
-            .def("setInitialPose", &Rtabmap::setInitialPose)
+            .def("setMemoryThreshold", &Rtabmap::setMemoryThreshold, "maxMemoryAllowed"_a)
+            .def("setInitialPose", &Rtabmap::setInitialPose, "initialPose"_a)
             .def("triggerNewMap", &Rtabmap::triggerNewMap)
-            .def("labelLocation", &Rtabmap::labelLocation)
-            .def("setUserData", &Rtabmap::setUserData)
-            .def("generateDOTGraph", &Rtabmap::generateDOTGraph)
-            .def("exportPoses", &Rtabmap::exportPoses)
+            .def("labelLocation", &Rtabmap::labelLocation, "id"_a, "label"_a)
+            .def("setUserData", &Rtabmap::setUserData, "id"_a, "data"_a)
+            .def("generateDOTGraph", &Rtabmap::generateDOTGraph, "path"_a, "id"_a = 0, "margin"_a = 5)
+            .def("exportPoses", &Rtabmap::exportPoses, "path"_a, "optimized"_a, "globals"_a, "format"_a)
             .def("resetMemory", &Rtabmap::resetMemory)
             .def("dumpPrediction", &Rtabmap::dumpPrediction)
             .def("dumpData", &Rtabmap::dumpData)
-            .def("parseParameters", &Rtabmap::parseParameters)
+            .def("parseParameters", &Rtabmap::parseParameters, "parameters"_a)
             .def("getParameters", &Rtabmap::getParameters)
-            .def("setWorkingDirectory", &Rtabmap::setWorkingDirectory)
+            .def("setWorkingDirectory", &Rtabmap::setWorkingDirectory, "path"_a)
             .def("rejectLastLoopClosure", &Rtabmap::rejectLastLoopClosure)
             .def("deleteLastLocation", &Rtabmap::deleteLastLocation)
             // .def("setOptimizedPoses", &Rtabmap::setOptimizedPoses)
-            .def("getSignatureCopy", &Rtabmap::getSignatureCopy)
+            .def("getSignatureCopy", &Rtabmap::getSignatureCopy, "id"_a, "images"_a, "scan"_a, "userData"_a,
+                 "occupancyGrid"_a, "withWords"_a, "withGlobalDescriptors"_a)
             // .def("getGraph", &Rtabmap::getGraph)
             .def("getNodesInRadius",
                  nb::overload_cast<const Transform &, float, int, std::map<int, float> *>(&Rtabmap::getNodesInRadius),
@@ -122,16 +124,20 @@ NB_MODULE(rtab_ext, m) {
             .def("getNodesInRadius",
                  nb::overload_cast<int, float, int, std::map<int, float> *>(&Rtabmap::getNodesInRadius), "nodeId"_a,
                  "radius"_a, "k"_a, "distsSqr"_a)
-            .def("detectMoreLoopClosures", &Rtabmap::detectMoreLoopClosures)
-            .def("globalBundleAdjustment", &Rtabmap::globalBundleAdjustment)
-            .def("cleanupLocalGrids", &Rtabmap::cleanupLocalGrids)
+            .def("detectMoreLoopClosures", &Rtabmap::detectMoreLoopClosures, "clusterRadiusMax"_a = 0.5f,
+                 "clusterAngle"_a = M_PI / 6.0f, "iterations"_a = 1, "intraSession"_a = true, "interSession"_a = true,
+                 "state"_a = 0, "clusterRadiusMin"_a = 0.0f)
+            .def("globalBundleAdjustment", &Rtabmap::globalBundleAdjustment, "optimizerType"_a = 1 /*g2o*/,
+                 "rematchFeatures"_a = true, "iterations"_a = 0, "pixelVariance"_a = 0.0f)
+            .def("cleanupLocalGrids", &Rtabmap::cleanupLocalGrids, "mapPoses"_a, "map"_a, "xMin"_a, "yMin"_a,
+                 "cellSize"_a, "cropRadius"_a = 1, "filterScans"_a = false)
             .def("refineLinks", &Rtabmap::refineLinks)
-            .def("addLink", &Rtabmap::addLink)
-            .def("getInformation", &Rtabmap::getInformation)
-            .def("addNodesToRepublish", &Rtabmap::addNodesToRepublish)
+            .def("addLink", &Rtabmap::addLink, "link"_a)
+            .def("getInformation", &Rtabmap::getInformation, "covariance"_a)
+            .def("addNodesToRepublish", &Rtabmap::addNodesToRepublish, "ids"_a)
             .def("getPathStatus", &Rtabmap::getPathStatus)
-            .def("clearPath", &Rtabmap::clearPath)
-            .def("computePath", nb::overload_cast<int, bool>(&Rtabmap::computePath), "targetNode"_a, "global"_a)
+            .def("clearPath", &Rtabmap::clearPath, "status"_a)
+            .def("computePath", nb::overload_cast<int, bool>(&Rtabmap::computePath), "targetNode"_a, "globals"_a)
             .def("computePath", nb::overload_cast<const Transform &, float>(&Rtabmap::computePath), "targetPose"_a,
                  "tolerance"_a)
             .def("getPath", &Rtabmap::getPath)
@@ -141,9 +147,10 @@ NB_MODULE(rtab_ext, m) {
             .def("getPathCurrentIndex", &Rtabmap::getPathCurrentIndex)
             .def("getPathCurrentGoalIndex", &Rtabmap::getPathCurrentGoalIndex)
             .def("getPathTransformToGoal", &Rtabmap::getPathTransformToGoal)
-            .def("getForwardWMPoses", &Rtabmap::getForwardWMPoses)
-            .def("getPaths", &Rtabmap::getPaths)
-            .def("adjustLikelihood", &Rtabmap::adjustLikelihood);
+            .def("getForwardWMPoses", &Rtabmap::getForwardWMPoses, "fromId"_a, "maxNearestNeighbors"_a, "radius"_a,
+                 "maxDiffID"_a)
+            .def("getPaths", &Rtabmap::getPaths, "poses"_a, "target"_a, "maxGraphDepth"_a = 0)
+            .def("adjustLikelihood", &Rtabmap::adjustLikelihood, "likelihood"_a);
     // .def("selectHypothesis", &Rtabmap::selectHypothesis);
 
     nb::class_<ProgressState>(m, "ProgressState")
