cc049864d135e63eb44c8b372773d3792172b754
yangfengzzz <yangfengzzz@hotmail.com>
2025-10-01 07:23:53 +0800

    pass python test

--- src/py_rtab/__init__.py
+++ src/py_rtab/__init__.py
@@ -2,19 +2,15 @@ import ctypes
 import os
 import sys
 
-# install_path = os.path.join(os.path.dirname(__file__), '../..')
-# postfix = ""
-# if sys.platform.startswith('linux'):
-#     postfix = ".so"
-#     ctypes.CDLL("libcuda.so", ctypes.RTLD_GLOBAL)
-#     ctypes.CDLL(install_path + "/libusd_hd" + postfix, ctypes.RTLD_LOCAL)
-#     ctypes.CDLL(install_path + "/libusd_usdHydra" + postfix, ctypes.RTLD_LOCAL)
-#     ctypes.CDLL(install_path + "/libusd_usdPhysicsImaging" + postfix, ctypes.RTLD_LOCAL)
-#
-# if sys.platform.startswith('darwin'):
-#     postfix = ".dylib"
-#
-# ctypes.CDLL(install_path + "/libusd_boost" + postfix, ctypes.RTLD_LOCAL)
+install_path = os.path.join(os.path.dirname(__file__), '')
+postfix = ""
+if sys.platform.startswith('linux'):
+    postfix = ".so"
 
+if sys.platform.startswith('darwin'):
+    postfix = ".dylib"
+
+ctypes.CDLL(install_path + "/librtabmap_utilite" + postfix, ctypes.RTLD_LOCAL)
+ctypes.CDLL(install_path + "/librtabmap_core" + postfix, ctypes.RTLD_LOCAL)
 
 from .rtab_ext import *
