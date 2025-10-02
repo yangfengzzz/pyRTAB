67d0d49cd9d811ab924a094605d9e74482f5f558
yangfengzzz <yangfengzzz@hotmail.com>
2025-10-02 12:06:10 +0800

    pass python test

--- src/py_visual_word.cpp
+++ src/py_visual_word.cpp
@@ -14,13 +14,13 @@ using namespace rtabmap;
 void bindVisualWord(nb::module_ &m) {
     nb::class_<VisualWord>(m, "VisualWord")
             .def(nb::init<int, const cv::Mat &, int>(), "id"_a, "descriptor"_a, "signatureId"_a = 0)
-            .def("addRef", &VisualWord::addRef)
-            .def("removeAllRef", &VisualWord::removeAllRef)
+            .def("addRef", &VisualWord::addRef, "signatureId"_a)
+            .def("removeAllRef", &VisualWord::removeAllRef, "signatureId"_a)
             .def("getMemoryUsed", &VisualWord::getMemoryUsed)
             .def("getTotalReferences", &VisualWord::getTotalReferences)
             .def("id", &VisualWord::id)
             .def("getDescriptor", &VisualWord::getDescriptor)
             .def("getReferences", &VisualWord::getReferences)
             .def("isSaved", &VisualWord::isSaved)
-            .def("setSaved", &VisualWord::setSaved);
+            .def("setSaved", &VisualWord::setSaved, "saved"_a);
 }
