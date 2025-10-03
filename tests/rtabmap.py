776dc0bc02143ac1df166eeb432ffd7464a44a8c
yangfengzzz <yangfengzzz@hotmail.com>
2025-10-03 10:26:25 +0800

    pass python test

--- tests/rtabmap.py
+++ tests/rtabmap.py
@@ -1,8 +1,22 @@
 import py_rtab
 
 if __name__ == '__main__':
-    odom = py_rtab.Odometry.create()
-    rtabmap = py_rtab.Rtabmap()
-    rtabmap.init()
+    camera = py_rtab.CameraRealSense2("")
+    if camera.init():
+        odom = py_rtab.Odometry.create()
+        rtabmap = py_rtab.Rtabmap()
+        rtabmap.init()
 
-    info = py_rtab.OdometryInfo()
+        data = camera.takeImage()
+        while data.isValid():
+            info = py_rtab.OdometryInfo()
+            pose = odom.process(data, info)
+
+            if rtabmap.process(data, pose):
+                if rtabmap.getLoopClosureId() > 0:
+                    print("Loop closure detected!\n")
+
+            data = camera.takeImage()
+        print("Processed all frames\n")
+    else:
+        print("Failed to initialize camera!\n")
