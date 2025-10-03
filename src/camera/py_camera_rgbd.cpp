2e59a3bc4a6463f2105430371659726eb3a07ea6
yangfengzzz <yangfengzzz@hotmail.com>
2025-10-03 10:16:58 +0800

    pass python test

--- src/camera/py_camera_rgbd.cpp
+++ src/camera/py_camera_rgbd.cpp
@@ -24,4 +24,30 @@ void bindCameraRGBD(nb::module_& m) {
             .def("init", &CameraRGBDImages::init, "calibrationFolder"_a, "cameraName"_a = 0)
             .def("setStartIndex", &CameraRGBDImages::setStartIndex, "index"_a)
             .def("setMaxFrames", &CameraRGBDImages::setMaxFrames, "value"_a);
+
+    nb::class_<CameraRealSense2, Camera>(m, "CameraRealSense2")
+            .def(nb::init<const std::string&, float>(), "deviceId"_a = "", "imageRate"_a = 0)
+            .def("init", &CameraRealSense2::init, "calibrationFolder"_a = ".", "cameraName"_a = "")
+            .def("isCalibrated", &CameraRealSense2::isCalibrated)
+            .def("getSerial", &CameraRealSense2::getSerial)
+            .def("odomProvided", &CameraRealSense2::odomProvided)
+            .def(
+                    "getPose",
+                    [](CameraRealSense2* self, double stamp, double maxWaitTime) {
+                        Transform pose;
+                        cv::Mat covariance;
+                        auto result = self->getPose(stamp, pose, covariance, maxWaitTime);
+                        return std::make_tuple(result, pose, covariance);
+                    },
+                    "stamp"_a, "maxWaitTime"_a = 0.06)
+            .def("setEmitterEnabled", &CameraRealSense2::setEmitterEnabled, "enabled"_a)
+            .def("setIRFormat", &CameraRealSense2::setIRFormat, "enabled"_a, "useDepthInsteadOfRightImage"_a)
+            .def("setResolution", &CameraRealSense2::setResolution, "width"_a, "height"_a, "fps"_a = 30)
+            .def("setDepthResolution", &CameraRealSense2::setDepthResolution, "width"_a, "height"_a, "fps"_a = 30)
+            .def("setGlobalTimeSync", &CameraRealSense2::setGlobalTimeSync, "enabled"_a)
+            .def("setDualMode", &CameraRealSense2::setDualMode, "enabled"_a, "extrinsics"_a)
+            .def("setJsonConfig", &CameraRealSense2::setJsonConfig, "json"_a)
+            .def("setImagesRectified", &CameraRealSense2::setImagesRectified, "enabled"_a)
+            .def("setOdomProvided", &CameraRealSense2::setOdomProvided, "enabled"_a, "imageStreamsDisabled"_a = false,
+                 "onlyLeftStream"_a = false);
 }
